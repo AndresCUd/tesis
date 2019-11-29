3548f8d874b29243614826a979a3c4ea6b84e603
AndresCUd <43498268+AndresCUd@users.noreply.github.com>
2019-11-29 08:31:56 -0500

    grficas

---
+++ datos/Prubeas mode = 2/graficas.py
@@ -0,0 +1,10 @@
+import os
+
+
+nodo1 = open("C://Users//andres//Documents//GitHub//tesis//datos//Prubeas mode = 2//nodo1.txt","r")
+nodo3 = open("C://Users//andres//Documents//GitHub//tesis//datos//Prubeas mode = 2//nodo3.txt","r")
+nodo5 = open("C://Users//andres//Documents//GitHub//tesis//datos//Prubeas mode = 2//nodo5.txt","r")
+
+data1=nodo1.read()
+data2=nodo3.read()
+data3=nodo5.read()
