23cc657228ae2fef6550e88492b48b20d0ebcd90
AndresCUd <acgonzalezh@correo.udistrital.edu.co>
2020-07-06 13:14:09 -0500

    requirements.txt

--- lora/settings.py
+++ lora/settings.py
@@ -98,9 +98,6 @@ DATABASES = {
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
