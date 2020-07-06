fb3692eefd24c57e2308398ebd73f5dfe854e1e2
AndresCUd <acgonzalezh@correo.udistrital.edu.co>
2020-07-06 16:20:06 -0500

    ccs

--- lora/settings.py
+++ lora/settings.py
@@ -121,7 +121,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT =os.path.join(BASE_DIR,"staticfiles")
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
 import django_heroku
