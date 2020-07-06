0bc2476fe7f425dc4e293e34f2345f529ee0d72a
AndresCUd <acgonzalezh@correo.udistrital.edu.co>
2020-07-06 16:28:39 -0500

    ccs

--- lora/settings.py
+++ lora/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q$d8#y5b$v%o%xhln$gn_77mk1ye7p!-#4_m*iz^e1zdnuew)0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://lora-ud.herokuapp.com/']
 
