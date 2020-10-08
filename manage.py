62be6e67236ef54ff8597d18a0b7562daffa511a
AndresCUd <43498268+AndresCUd@users.noreply.github.com>
2020-10-08 13:31:08 -0500

    b

--- manage.py
+++ manage.py
@@ -11,7 +11,7 @@ def main():
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
+            
             
             ) from exc
     execute_from_command_line(sys.argv)
